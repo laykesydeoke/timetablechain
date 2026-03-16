165bd12139e28b3ddae39c48523f8e64e79d2976
Olalekan <aoakande2016@gmail.com>
2026-03-16 14:08:35 +0100

    Add template_t1

---
+++ tests/template_t1.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest";
+import { Cl } from "@stacks/transactions";
+import { simnet } from "./setup";
+describe("template 1", () => {
+  it("test 1", () => {
+    const r = simnet.callReadOnlyFn("timetablechain", "get-template-stats", [], simnet.deployer);
+    expect(r.result).not.toBeNone();
+  });
+});
