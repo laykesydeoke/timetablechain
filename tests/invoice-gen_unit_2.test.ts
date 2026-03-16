7cf3e4395edf3e93c1b12c6b6d69282dd9dbb145
Olalekan <aoakande2016@gmail.com>
2026-03-16 13:03:00 +0100

    test(invoice-gen): checks active status #2

---
+++ tests/invoice-gen_unit_2.test.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import { Cl } from "@stacks/transactions";
+import { simnet } from "./setup";
+const deployer = simnet.deployer;
+
+describe("invoice-gen unit test 2: checks active status", () => {
+  it("checks active status for invoice-gen module", () => {
+    const r=simnet.callReadOnlyFn("timetablechain","is-invoice-gen-active",[Cl.uint(2)],deployer);expect(r.result).toBeBool(false);
+  });
+});
