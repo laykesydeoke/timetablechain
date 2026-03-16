a2aa8ad7a86b771da8c0c2e0b3a4fc8fd32a8c38
Olalekan <aoakande2016@gmail.com>
2026-03-16 14:08:43 +0100

    Add exhist_t15

---
+++ tests/exhist_t15.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest";
+import { Cl } from "@stacks/transactions";
+import { simnet } from "./setup";
+describe("exhist 15", () => {
+  it("test 15", () => {
+    const r = simnet.callReadOnlyFn("timetablechain", "get-exchange-history-params", [], simnet.deployer);
+    expect(r.result).not.toBeNone();
+  });
+});
