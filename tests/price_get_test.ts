1cd9796ef7e91b1f1029008c8c48f7328fdcbdfc
Olalekan <aoakande2016@gmail.com>
2026-03-16 14:06:26 +0100

    Add price_get_test

---
+++ tests/price_get_test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest";
+import { Cl } from "@stacks/transactions";
+import { simnet } from "./setup";
+describe("get slot price returns data", () => {
+  it("get slot price returns data", () => {
+    const r = simnet.callReadOnlyFn("timetablechain", "get-pricing-params", [], simnet.deployer);
+    expect(r.result).not.toBeNone();
+  });
+});
