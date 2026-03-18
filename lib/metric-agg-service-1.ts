a681240e9253236a057ec07de5cc3c8aaa222c5c
Olalekan <aoakande2016@gmail.com>
2026-03-18 08:23:00 +0100

    lib(metric-agg): add service module 1

---
+++ lib/metric-agg-service-1.ts
@@ -0,0 +1,14 @@
+import { metric_agg_CONFIG } from '../utils/metric-agg-constants-1';
+
+export class metricaggService1 {
+  private contractName: string;
+  constructor(contractName = metric_agg_CONFIG.contractName) { this.contractName = contractName; }
+
+  async getCount(): Promise<number> { return 0; }
+  async getEntry(id: number): Promise<{value: number; active: boolean} | null> { return null; }
+  async create(value: number): Promise<number> {
+    if (value <= 0) throw new Error('Value must be positive');
+    return 0;
+  }
+  isValidId(id: number): boolean { return Number.isInteger(id) && id > 0; }
+}
