6acfd173800e999d968ebb9bf73db030c4680449
Olalekan <aoakande2016@gmail.com>
2026-03-19 08:23:00 +0100

    lib(seed-data): add service module 1

---
+++ lib/seed-data-service-1.ts
@@ -0,0 +1,14 @@
+import { seed_data_CONFIG } from '../utils/seed-data-constants-1';
+
+export class seeddataService1 {
+  private contractName: string;
+  constructor(contractName = seed_data_CONFIG.contractName) { this.contractName = contractName; }
+
+  async getCount(): Promise<number> { return 0; }
+  async getEntry(id: number): Promise<{value: number; active: boolean} | null> { return null; }
+  async create(value: number): Promise<number> {
+    if (value <= 0) throw new Error('Value must be positive');
+    return 0;
+  }
+  isValidId(id: number): boolean { return Number.isInteger(id) && id > 0; }
+}
