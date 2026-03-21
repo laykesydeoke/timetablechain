1388792925f53f893432d5d71d7f71f0adf35cac
Olalekan <aoakande2016@gmail.com>
2026-03-21 15:04:40 +0100

    test(asset-pipe): checks active status #7

---
+++ tests/asset-pipe_unit_7.test.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import { Cl } from "@stacks/transactions";
+import { simnet } from "./setup";
+const deployer = simnet.deployer;
+
+describe("asset-pipe unit test 7: checks active status", () => {
+  it("checks active status for asset-pipe module", () => {
+    const r=simnet.callReadOnlyFn("timetablechain","is-asset-pipe-active",[Cl.uint(7)],deployer);expect(r.result).toBeBool(false);
+  });
+});
