2aa2b8d7d6469a078ec39f609d7d8d2b3604a49d
Olalekan <aoakande2016@gmail.com>
2026-03-23 07:53:20 +0100

    lib(currency-fmt): add service module 2

---
+++ lib/currency-fmt-service-2.ts
@@ -0,0 +1,14 @@
+import { currency_fmt_CONFIG } from '../utils/currency-fmt-constants-1';
+
+export class currencyfmtService2 {
+  private contractName: string;
+  constructor(contractName = currency_fmt_CONFIG.contractName) { this.contractName = contractName; }
+
+  async getCount(): Promise<number> { return 0; }
+  async getEntry(id: number): Promise<{value: number; active: boolean} | null> { return null; }
+  async create(value: number): Promise<number> {
+    if (value <= 0) throw new Error('Value must be positive');
+    return 0;
+  }
+  isValidId(id: number): boolean { return Number.isInteger(id) && id > 0; }
+}
