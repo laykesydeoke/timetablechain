2a2ecb033e30633f82d4e798291ec09b30c249a2
Olalekan <aoakande2016@gmail.com>
2026-03-23 05:08:00 +0100

    test(i18n-svc): checks active status #17

---
+++ tests/i18n-svc_unit_17.test.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import { Cl } from "@stacks/transactions";
+import { simnet } from "./setup";
+const deployer = simnet.deployer;
+
+describe("i18n-svc unit test 17: checks active status", () => {
+  it("checks active status for i18n-svc module", () => {
+    const r=simnet.callReadOnlyFn("timetablechain","is-i18n-svc-active",[Cl.uint(17)],deployer);expect(r.result).toBeBool(false);
+  });
+});
